fa0c194e89bfd48e12359444f1ff7b7865aaca14
Silvio Amir <silvio.aam@gmail.com>
2017-09-02 20:20:41 -0400

    improved UIX

--- Sleek@Slack/scripts/run_dev.sh
+++ Sleek@Slack/scripts/run_dev.sh
@@ -3,7 +3,7 @@ API_TOKEN_ID="SLEEK_DEV_TOKEN"
 INIT=0
 if (($INIT == 1 )); then
 	echo "INITING BACKEND"
-	python sleek@slack.py -cfg $CONFIG -init -surveys DATA/surveys/
+	python main.py -cfg $CONFIG -init -surveys DATA/surveys/
 else
-	python sleek@slack.py -cfg $CONFIG -api_token_id $API_TOKEN_ID -connect -dbg 
+	python main.py -cfg $CONFIG -api_token_id $API_TOKEN_ID -connect -dbg 
 fi
